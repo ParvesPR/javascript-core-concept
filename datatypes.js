52e95afece9301e65a816ac9598c6f08007a724f
ParvesPR <parves2585@gmail.com>
2024-02-06 13:19:15 +0800

    number data types declaration

--- datatypes.js
+++ datatypes.js
@@ -1,16 +1,28 @@
-Primitive
-Number
+// Primitive
+// Number
 // 10, 10.5
-String
-'This is string' + 'This is string'
-Boolean
+// String
+// 'This is string' + 'This is string'
+// Boolean
 // true, false
-undefined
+// undefined
 // missing data
-null
+// null
 // data does not exist
 
-Object
-Array
-object
-function
+// Object
+// Array
+// Object
+// Function
+
+// ******Number******
+var a = 10;
+var b = 10.003;
+const ab = Number('12')
+const abc = Number('12.22')
+console.log(Number.parseFloat(ab));
+console.log(Number.parseInt(abc));
+console.log(Number.MAX_VALUE);
+console.log(Number.MIN_SAFE_INTEGER);
+console.log(1/0); Infinity
+console.log('abcd' * 1); NaN
