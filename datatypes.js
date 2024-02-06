4c7bfa49c82f7f8def148225da9e19c5bf8f65af
ParvesPR <parves2585@gmail.com>
2024-02-06 12:51:31 +0800

    data types in JavaScript

---
+++ datatypes.js
@@ -0,0 +1,16 @@
+Primitive
+Number
+// 10, 10.5
+String
+'This is string' + 'This is string'
+Boolean
+// true, false
+undefined
+// missing data
+null
+// data does not exist
+
+Object
+Array
+object
+function
