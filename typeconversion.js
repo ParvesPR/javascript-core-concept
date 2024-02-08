2b4560e1e2a5764834cc684b546d5ace6b424a5c
ParvesPR <parves2585@gmail.com>
2024-02-08 13:35:06 +0800

    added javascript falsy value

--- typeconversion.js
+++ typeconversion.js
@@ -2,3 +2,15 @@ var str = '1100';
 var n = 10;
 console.log(Number.parseInt(str) + n);
 console.log(n.toString());
+console.log(Boolean(Infinity))
+
+// Falsy value
+''
+0
+undefined
+null
+NaN
+console.log(Boolean(''));
+console.log(Boolean(undefined));
+console.log(Boolean(null));
+console.log(Boolean(NaN));
