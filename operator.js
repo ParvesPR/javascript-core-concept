17487f72e7dd526d5515c66967bd962e8c8157bd
ParvesPR <parves2585@gmail.com>
2024-02-12 13:25:25 +0800

    js arithmetic operator

---
+++ operator.js
@@ -0,0 +1,13 @@
+// Arithmetic Operator
+// + - * / %
+var a = 10;
+var b = 6;
+// console.log(a % b);
+
+/* Incremental 
+pre-incremental  post-incremental
+*/
+console.log(a++);
+console.log(b--);
+console.log(a);
+console.log(b);
